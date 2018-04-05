4da392e33ac8ad2028ec171b3dd124cd25c9c725
user138841 <user138841@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-04-05 16:35:11 -0400

    Stephen work on a quiz app could not complete 05/05

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -8,17 +8,64 @@
 
 import UIKit
 
+struct Question {
+    //This is creating a structure, a structure is like a class but it does not have inhertince
+    var Question : String!
+    var Answers : [String]!
+    var Answer : Int!
+    //var means variable
+}
+
+
+
+
 class ViewController: UIViewController {
 
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    
+    var Questions = [Question]()
+    
+    var QNumber = Int()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Questions = [Question(Question: "What is a structure in Swift?", Answers: ["Its a class", "Its a function", "It is like a class, but not the same", "It is a getter and setter"], Answer: 2)]
+        
+        QuestionPick()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func QuestionPick(){
+        
+        if Questions.count > 0{
+            
+            QNumber = 0
+            questionLabel.text = Questions[QNumber].Question
+            
+            for i in 0..<answerButtons.count{
+                answerButtons[i].setTitle(Questions[QNumber].Answers[i], for: UIControlState.normal)
+            }
+            
+            Questions.remove(at: QNumber)
+            
+        }
+        else{
+            
+            NSLog("Done!")
+            
+        }
+        
+    }
+    
 
 
 }
